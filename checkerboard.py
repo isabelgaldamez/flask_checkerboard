fd42e9fb6fec18e417a6625a36c577c0f10abb8c
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2019-09-08 19:03:25 -0700

    updated route to use 4 parameters which includes apdate colors on the board

--- checkerboard.py
+++ checkerboard.py
@@ -9,6 +9,8 @@ def default_board_times(vertical):
 @app.route('/<int:horizontal>/<int:vertical>')
 def default_board_by(horizontal, vertical):
     return render_template('index.html', display_horizontal=int(horizontal/2), display_vertical=int(vertical/2))
-
+@app.route('/<int:horizontal>/<int:vertical>/<color1>/<color2>')
+def board_update_colors(horizontal, vertical, color1, color2):
+    return render_template('index.html', display_horizontal=int(horizontal/2), display_vertical=int(vertical/2), color_a = color1, color_b = color2)
 if __name__ == '__main__':
     app.run(debug = True)
