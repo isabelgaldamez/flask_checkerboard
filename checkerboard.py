f1bf097fe2bf5dcffe2742eb3f5e911397addb0e
Isabel Galdamez <isabelgaldamez@sbcglobal.net>
2019-09-08 18:49:23 -0700

    checkerboard using flask, python and jinja

---
+++ checkerboard.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, render_template
+app = Flask(__name__)
+@app.route('/')
+def default_board():
+    return render_template('index.html')
+@app.route('/<int:vertical>')
+def default_board_times(vertical):
+    return render_template('index.html', display_vertical=vertical)
+@app.route('/<int:horizontal>/<int:vertical>')
+def default_board_by(horizontal, vertical):
+    return render_template('index.html', display_horizontal=int(horizontal/2), display_vertical=int(vertical/2))
+
+if __name__ == '__main__':
+    app.run(debug = True)
